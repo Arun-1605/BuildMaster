3a7fcd35a2523c49da01d2b46ea98a4b5d7b9746
Arun-1605 <aarun160599@gmail.com>
2026-04-11 02:10:06 +0530

    Change API endpoint from HTTP to HTTPS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://buildmaster.runasp.net/api',
+  apiBaseUrl: 'https://buildmaster.runasp.net/api',
   razorpayKeyId: ''     // set your production Razorpay key here
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://buildmaster.runasp.net/api',
+  apiBaseUrl: 'https://buildmaster.runasp.net/api',
   razorpayKeyId: 'rzp_test_SbTYDtPiDDBqdg'
 };
